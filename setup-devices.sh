18a28c2950082aee40b78f7c0241bad1772d754e
Michele Segata <segata@ccs-labs.org>
2017-05-04 15:46:21 +0200

    add script for setting up requirements on testbed nodes

---
+++ setup-devices.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# determine whether we are on a NUC or on a TPLINK
+opkg_path=`which opkg`
+if [ "$opkg_path" == "" ]; then
+	tplink=0
+	SUDO=sudo
+else
+	tplink=1
+	SUDO=""
+fi
+
+if [ $tplink -eq 1 ]; then
+	opkg install python python-pip
+else
+	sudo apt-get install python python-pip
+	sudo pip install setuptools
+fi
+${SUDO} pip install pyric pyroute2 ipaddr
+
+# patch a mistake in the official release of pyric
+if [ $tplink -eq 1 ]; then
+	sed -i -e "s/'sys/'\/sys/g" /usr/lib/python2.7/site-packages/pyric/utils/rfkill.py
+fi
