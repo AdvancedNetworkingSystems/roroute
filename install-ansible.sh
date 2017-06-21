93310137d57c1cfcfe5c480457bc8865d94f8f31
Michele Segata <segata@ccs-labs.org>
2017-06-21 13:44:53 +0200

    let the install-ansible.sh script be less verbose

--- install-ansible.sh
+++ install-ansible.sh
@@ -3,5 +3,7 @@
 ansible_path=`which ansible-playbook`
 
 if [ "$ansible_path" == "" ]; then
-	sudo apt-get install -y ansible
+	echo "Installing ansible on master node..."
+	sudo apt-get install -y ansible &> /dev/null
+	echo "Done"
 fi
