5624d47d6d1a8efabf171bb857bee9740e5021f4
Michele Segata <segata@ccs-labs.org>
2017-07-13 13:28:19 +0200

    avoid adding default route if existing when starting olsrd2

--- start-olsr.sh
+++ start-olsr.sh
@@ -12,8 +12,11 @@ if [ -z "$opkg_path" ]; then
 	sudo service olsrd2 stop
 	sudo service olsrd2 start
 	if [ "$testbed" = "wilab" ]; then
-		sudo ip -6 route add fe80::ec4:7aff:fe6c:a69a dev eno1
-		sudo ip -6 route add default via fe80::ec4:7aff:fe6c:a69a dev eno1
+		def=`ip -6 r | grep 'default via fe80::ec4:7aff:fe6c:a69a'`
+		if [ "$def" = "" ]; then
+			sudo ip -6 route add fe80::ec4:7aff:fe6c:a69a dev eno1
+			sudo ip -6 route add default via fe80::ec4:7aff:fe6c:a69a dev eno1
+		fi
 	fi
 else
 	/etc/init.d/olsrd2 stop
