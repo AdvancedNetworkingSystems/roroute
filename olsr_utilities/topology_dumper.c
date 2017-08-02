dec1cb214448201a624e51fff06e8d27f4f449d4
Michele Segata <segata@ccs-labs.org>
2017-08-02 11:03:25 +0200

    change topology dumper to dump routing table and intervals as well

--- olsr_utilities/topology_dumper.c
+++ olsr_utilities/topology_dumper.c
@@ -4,21 +4,22 @@
 
 #define HOST "127.0.0.1"
 #define PORT 2009
-#define REQ "/netjsoninfo filter graph ipv4_0/quit\n"
+#define REQ_TOPOLOGY "/netjsoninfo filter graph ipv4_0/quit\n"
+#define REQ_ROUTE "/netjsoninfo filter route ipv4_0/quit\n"
+#define REQ_HELLO "/config get interface.hello_interval/quit\n"
+#define REQ_TC "/config get olsrv2.tc_interval/quit\n"
+#define EXT_TOPO ".topo"
+#define EXT_ROUTE ".route"
+#define EXT_INTERVAL ".int"
 
-/**
- * Get the topology data from host
- * @param oonf plugin handler object
- * @return 1 if success, 0 otherwise
- */
-char* get_topology()
+char* get_olsr_data(const char *query)
 {
 	int sd, sent;
 	char *recv_buffer;
 	if((sd = _create_socket(HOST, PORT))==0){
 		return 0;
 	}
-	if( (sent = send(sd, REQ, strlen(REQ), MSG_NOSIGNAL))==-1){
+	if( (sent = send(sd, query, strlen(query), MSG_NOSIGNAL))==-1){
 		close(sd);
 		return 0;
 	}
@@ -30,9 +31,19 @@ char* get_topology()
 	return recv_buffer;
 }
 
+void remove_newline(char *str) {
+	int pos = 0;
+	while (str[pos] != 0 && str[pos] != '\n')
+		pos++;
+	if (str[pos] == 0)
+		return;
+	for (int i = pos; i < strlen(str); i++)
+		str[i] = str[i+1];
+}
+
 int main(int argc, char **argv) {
 
-	char *topology;
+	char *topology, *routing_table, *hello, *tc;
 	struct timespec start_time;
 	struct timespec stop_time;
 	struct timespec interval;
@@ -40,8 +51,8 @@ int main(int argc, char **argv) {
 	struct timespec wait_time;
 	struct timespec now;
 	struct timespec till_end;
-	FILE *out;
-	char filename[1024];
+	FILE *topo_out, *route_out, *interval_out;
+	char base_filename[1024], topo_filename[1024], route_filename[1024], interval_filename[1024];
 	uint64_t argv_start_time, argv_stop_time, argv_interval;
 	char *argv_id;
 	int i;
@@ -72,15 +83,40 @@ int main(int argc, char **argv) {
 		timespec_get(&now, TIME_UTC);
 		timespec_diff(&now, &next, &wait_time);
 		nanosleep(&wait_time, 0);
-		topology = get_topology();
+		topology = get_olsr_data(REQ_TOPOLOGY);
+		routing_table = get_olsr_data(REQ_ROUTE);
+		hello = get_olsr_data(REQ_HELLO);
+		remove_newline(hello);
+		tc = get_olsr_data(REQ_TC);
+		remove_newline(tc);
+
 		timespec_get(&now, TIME_UTC);
-		sprintf(filename, "%s_%06d_%lld_%09lld", argv_id, i, (long long)now.tv_sec, (long long)now.tv_nsec);
-		out = fopen(filename, "w");
+		sprintf(base_filename, "%s_%06d_%lld_%09lld", argv_id, i, (long long)now.tv_sec, (long long)now.tv_nsec);
+		sprintf(topo_filename, "%s%s", base_filename, EXT_TOPO);
+		sprintf(route_filename, "%s%s", base_filename, EXT_ROUTE);
+		sprintf(interval_filename, "%s%s", base_filename, EXT_INTERVAL);
+		topo_out = fopen(topo_filename, "w");
+		route_out = fopen(route_filename, "w");
+		interval_out = fopen(interval_filename, "w");
 		if (topology) {
-			fprintf(out, "%s", topology);
+			fprintf(topo_out, "%s", topology);
 			free(topology);
 		}
-		fclose(out);
+		if (routing_table) {
+			fprintf(route_out, "%s", routing_table);
+			free(routing_table);
+		}
+		if (hello) {
+			fprintf(interval_out, "%s", hello);
+			free(hello);
+		}
+		if (tc) {
+			fprintf(interval_out, "%s", tc);
+			free(tc);
+		}
+		fclose(topo_out);
+		fclose(route_out);
+		fclose(interval_out);
 		i++;
 		timespec_sum(&next, &interval, &next);
 		timespec_diff(&next, &stop_time, &till_end);
