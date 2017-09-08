882ac19c5a5e18e207e8c02efa878ba4ea5ca0c8
Nicolo' Facchi <nicolo.facchi@gmail.com>
2017-09-08 09:38:30 +0200

    Add scripts for switching off wifi on all nodes

---
+++ turnoff_wifi.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2017 Michele Segata <msegata@disi.unitn.it>
+# Copyright (C) 2017 Nicolo' Facchi <nicolo.facchi@unitn.it>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+#
+
+from pyric import pyw
+
+if __name__ == '__main__':
+
+    # Delete all Wi-Fi interfaces
+    wifi_int = pyw.winterfaces()
+    for wifi_int_name in wifi_int:
+        pyw.devdel(pyw.getcard(wifi_int_name))
---
+++ turnoff_wifi.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+$(which sudo) python turnoff_wifi.py
