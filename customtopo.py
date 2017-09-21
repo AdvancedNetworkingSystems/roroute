816dc8f403ef0f212d51fa21658571944a760147
Michele Segata <segata@ccs-labs.org>
2017-09-21 17:47:21 +0200

    change shortcut graph to have killable nodes in terms of highest centrality

--- customtopo.py
+++ customtopo.py
@@ -55,27 +55,32 @@ def ring_graph(n):
     return g
 
 
-def shortcut_graph(n, l):
+def shortcut_graph(n, l_odd, l_even):
     """
     Generate a graph with two chains are connected together in this way:
-    0 -- 1 -- 2 -------------- 3 -- 4 -- 5
-              |                |
-              6 - 7 - 8 - 9 - 10
+    0 -- 1 -- 2 ------ 3 ------ 4 -- 5 -- 6
+              |                 |
+              6 --- 7 --- 8 --- 9
+    In the example, the function is called with:
+    - n = 10
+    - l_odd = 1 (node number 3)
+    - l_even = 4 (nodes 6, 7, 8, and 9)
     :param n: total number of nodes
-    :param l: number of nodes in the lower chain. must be an odd number
+    :param l_odd: number of nodes in the odd chain (minimum 1)
+    :param l_even: number of nodes in the even chain (minimum 2)
     :return: a networkx graph with the given topology
     """
-    assert(l % 2 == 1)
-    assert(l > 3)
-    assert(n - l >= 2)
+    assert(l_odd % 2 == 1 and l_odd >= 1)
+    assert(l_even % 2 == 0 and l_even >= 2)
+    assert(n - l_even - l_odd >= 4)
     g = nx.empty_graph(n, create_using=nx.Graph())
     # create upper chain
-    for i in range(n - l - 1):
+    for i in range(n - l_even - 1):
         g.add_edge(i, i+1)
     # create lower chain
-    for i in range(n - l, n - 1):
+    for i in range(n - l_even, n - 1):
         g.add_edge(i, i+1)
     # connect the two chains together
-    g.add_edge((n-l)/2-1, n-l)
-    g.add_edge((n-l)/2, n-1)
+    g.add_edge((n-l_odd-2)/2-1, n-l_even)
+    g.add_edge((n-l_odd-2)/2+l_odd, n-1)
     return g
