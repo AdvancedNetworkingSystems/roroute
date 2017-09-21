b73b7d33af33882374dbfe0c3301b893276ef556
Michele Segata <segata@ccs-labs.org>
2017-09-21 11:24:14 +0200

    fix version of networkx to 1.11, which has OBVIOUSLY been upgraded to 2.0 in the night between the 20th and the 21st of september

--- setup-devices.sh
+++ setup-devices.sh
@@ -27,7 +27,8 @@ else
 	cd ../
 	${SUDO} pip install scipy
 fi
-${SUDO} pip install pyric pyroute2 ipaddr networkx netdiff
+${SUDO} pip install pyric pyroute2 ipaddr netdiff
+${SUDO} pip install networkx==1.11
 
 # patch a mistake in the official release of pyric
 if [ $tplink -eq 1 ]; then
