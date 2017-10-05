874f57bd2a37efce3201b463cea357cbedd43504
Michele Segata <segata@ccs-labs.org>
2017-10-05 10:48:14 +0200

    remove prince patching and use poprow branch

--- setup-prince.sh
+++ setup-prince.sh
@@ -8,32 +8,12 @@ if [ -z "$opkg_path" ]; then
 	if [ ! -d poprouting ]; then
 		git clone https://github.com/AdvancedNetworkingSystems/poprouting.git
 		cd poprouting
-		# git checkout f509b958ecde6d9c611bc39ffa7a25e2d95b7b7b
-		# git checkout b833f5417012702751ca03065ec937b43a5212c6
-		git checkout 8e2fed879b36bc0d0c66140b049a6a119ad649c4
-		cd ..
-	else
-		cd poprouting
-		make clean
-		git checkout -- .
+		git checkout poprow
+		make -j 4
 		cd ..
 	fi
 
 	cd poprouting
-
-	patch -p1 < ../prince.patch
-
-	# Fix LDFLAGS in Makefile (this was tested for commit 1b1fa863c4bb88d3)
-	# cat Makefile | sed \
-	# 	-e 's/\(^.*\)\$(LDFLAGS) \$(CFLAGS)\(.*$\)/\1 \$(CFLAGS) \2 \$(LDFLAGS)/' \
-	# 	-e 's/^CFLAGS\(.*$\)/LDFLAGS\1/' > Makefile.tmp && \
-	# 	mv Makefile.tmp Makefile
-
-	# # We use IPv4 not IPv6
-	cat prince/lib/OONF/oonf.c | sed -e 's/\(^.*\)ipv6_0\(.*$\)/\1ipv4_0\2/' > \
-		/tmp/oonf.c && mv /tmp/oonf.c prince/lib/OONF/oonf.c
-
-	make
 
 	if [ -L /usr/lib/libprince_oonf_c.so ]; then
 		sudo rm /usr/lib/libprince_oonf_c.so
