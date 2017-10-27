74579518e9f827a5488248269dd0c20e2dc4cf35
Michele Segata <segata@ccs-labs.org>
2017-10-27 07:23:57 +0200

    experiment timers plot: add degree of nodes, remove node ids and killed node

--- analysis_scripts/plot-experiment-timers.R
+++ analysis_scripts/plot-experiment-timers.R
@@ -5,7 +5,7 @@ source('generic-plot.R')
 #graphs params
 plot.width <- 308.43/72
 plot.height <- 184.546607/72/1.25
-plot.margin <- c(3.1, 3.3, 2.0, 0.4)
+plot.margin <- c(3.1, 3.3, 2.0, 3.3)
 leg.inset <- c(0, 0, 0, 0)
 
 plot.exp.timers <- function(outputFile, d, idx, xlab, leg.pos='topright', outputDir = './') {
@@ -18,15 +18,27 @@ plot.exp.timers <- function(outputFile, d, idx, xlab, leg.pos='topright', output
     d$k = 1:nrow(d)
 
     plot.new()
-    plot.window(xlim=c(0, nrow(d)+1), ylim=c(0, round(max(d$h, d$tc)) + 1.1), yaxs="i", xaxs="i")
+    xlim = c(0, nrow(d)+1)
+
+    limits = seq(0.2, 1, by=0.1)
+    y.max = min(limits[limits > max(d$expbc)])
+    plot.window(xlim=xlim, ylim=c(0, y.max), yaxs="i", xaxs="i")
+    points(d$k, d$expbc, col=2, lwd=2, pch=4, cex=.5)
+    mtext("centrality $b_i$", side=4, line=2.1)
+    axis(4, hadj=0, lwd=0, lwd.ticks=1, las=1)
+
+    par(new=T)
+
+    par(mar=plot.margin, xpd=F)
+    plot.window(xlim=xlim, ylim=c(0, round(max(d$h, d$tc)) + 1.1), yaxs="i", xaxs="i")
     abline(h=2, lwd=t.lwd("th"), col=t.col("th"), lty=t.lty("th"))
     abline(h=5, lwd=t.lwd("ta"), col=t.col("ta"), lty=t.lty("ta"))
     plot.serie(d$k, d$h   , pt.col=d$ap, serie="h")
     plot.serie(d$k, d$tc  , pt.col=d$ap, serie="tc")
-    kp = subset(d, killed==1)
-    points(kp$k, kp$h)
-    points(kp$k, kp$tc)
-    text(d$k, d$tc+0.5, gsub("nuc0-", "", d$node), cex=.5)
+    # kp = subset(d, killed==1)
+    # points(kp$k, kp$h)
+    # points(kp$k, kp$tc)
+    # text(d$k, d$tc+0.5, gsub("nuc0-", "", d$node), cex=.5)
 
     axis(1, padj=-.7, las=1, lwd=0, lwd.ticks=1)
     axis(2, hadj=.7, las=1, lwd=0, lwd.ticks=1)
@@ -38,14 +50,14 @@ plot.exp.timers <- function(outputFile, d, idx, xlab, leg.pos='topright', output
     par(xpd=T)
     legend(
         leg.pos,
-        legend=c('$t_H$', '$t_A$', '$t_H(i)$', '$t_A(i)$'),
-        ncol=2,
+        legend=c('$t_{\\texttt{H}}$', '$t_{\\texttt{A}}$', '$t_{\\texttt{H}}(i)$', '$t_{\\texttt{A}}(i)$', '$b_i$'),
+        ncol=3,
         inset=c(0, -0.3),
-        col    = t.col(c("th", "ta", "h", "tc")),
-        lty    = t.lty(c("th", "ta", "h", "tc")),
-        pch    = t.pch(c("th", "ta", "h", "tc")),
-        pt.cex = t.cex(c("th", "ta", "h", "tc")),
-        lwd    = t.lwd(c("th", "ta", "h", "tc")),
+        col    = c(t.col(c("th", "ta", "h", "tc")), 2),
+        lty    = c(t.lty(c("th", "ta", "h", "tc")), NA),
+        pch    = c(t.pch(c("th", "ta", "h", "tc")), 4),
+        pt.cex = c(t.cex(c("th", "ta", "h", "tc")), .5),
+        lwd    = c(t.lwd(c("th", "ta", "h", "tc")), 2),
         box.lwd=0,
         cex=.8,
         seg.len=2.5
