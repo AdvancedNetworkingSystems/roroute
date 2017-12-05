b02dd75fd357bcb5b42b7ed7fed921726aee2c3e
Michele Segata <segata@ccs-labs.org>
2017-12-05 17:38:19 +0100

    add scripts to install wishful on nodes

---
+++ setup-wishful.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# determine whether we are on a NUC or on a TPLINK
+opkg_path=`which opkg`
+if [ "$opkg_path" == "" ]; then
+	tplink=0
+else
+	tplink=1
+fi
+
+sudo apt-get install -y wget git python python-virtualenv python-dev python3-dev python3-pip rfkill
+sudo pip install setuptools
+sudo pip install pyric pyroute2 ipaddr
+
+git config --global user.name "ANS"
+git config --global user.email "ans@ans.disi.unitn.it"
+git config --global color.diff auto
+git config --global color.status auto
+export PATH=$PATH:$HOME/
+
+if [ -d wishful ]; then
+	cd wishful
+	repo forall -c 'git pull --rebase'
+else
+	wget https://storage.googleapis.com/git-repo-downloads/repo
+	chmod a+x ./repo
+	mkdir -p wishful
+	cd wishful
+	repo init -u https://ans.disi.unitn.it/redmine/manifests-wishful.git
+	repo init -m user.xml
+	repo sync
+	repo forall -c 'git checkout master'
+	virtualenv -p /usr/bin/python3 ./dev
+	source ./dev/bin/activate
+	pip3 install -U -r ./.repo/manifests/requirements.txt
+	cd ..
+fi
+
+# mkdir -p wishful
+# cd wishful
+# repo init -u https://github.com/wishful-project/manifests.git
+# repo init -m user.xml
+# repo sync
+# repo forall -c 'git checkout master'
+# virtualenv -p /usr/bin/python3 ./dev
+# source ./dev/bin/activate
+# pip3 install -U -r ./.repo/manifests/requirements.txt
+# cd ..
