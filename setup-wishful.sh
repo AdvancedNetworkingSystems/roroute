054e8da5173db4201b7ac512f8b25e0c8d53c3b5
Michele Segata <segata@ccs-labs.org>
2018-02-21 11:24:11 +0100

    change setup script for alix

--- setup-wishful.sh
+++ setup-wishful.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 
-# determine whether we are on a NUC or on a TPLINK
-opkg_path=`which opkg`
-if [ "$opkg_path" == "" ]; then
-	tplink=0
-else
-	tplink=1
-fi
-
-sudo apt-get install -y wget git python python-virtualenv python-dev python3-dev python3-pip rfkill
-sudo pip install setuptools
-sudo pip install pyric pyroute2 ipaddr
+sudo apt-get install -y wget git rfkill
 
 git config --global user.name "ANS"
 git config --global user.email "ans@ans.disi.unitn.it"
@@ -18,38 +8,25 @@ git config --global color.diff auto
 git config --global color.status auto
 export PATH=$PATH:$HOME/
 
-if [ -d wishful ]; then
-	cd wishful
-	repo forall -c 'git pull --rebase'
-	. ./dev/bin/activate
-	pip3 install -U -r ./.repo/manifests/requirements.txt
-	pip3 install pyric pyroute2 ipaddr netdiff scipy
-	pip3 install networkx==1.11
-	cd ..
-else
-	wget https://storage.googleapis.com/git-repo-downloads/repo
-	chmod a+x ./repo
-	mkdir -p wishful
-	cd wishful
-	repo init -u https://github.com/AdvancedNetworkingSystems/manifests-wishful
-	repo init -m user.xml
-	repo sync
-	repo forall -c 'git checkout master'
-	virtualenv -p /usr/bin/python3 ./dev
-	. ./dev/bin/activate
-	pip3 install -U -r ./.repo/manifests/requirements.txt
-	pip3 install pyric pyroute2 ipaddr netdiff scipy
-	pip3 install networkx==1.11
-	cd ..
-fi
+cd
+wget https://storage.googleapis.com/git-repo-downloads/repo
+chmod a+x ./repo
+mkdir -p wishful
+cd wishful
+repo init -u https://github.com/AdvancedNetworkingSystems/manifests-wishful
+cd manifests-wishful
+git checkout alix
+cd ..
+repo init -m user.xml
+repo sync
+repo forall -c 'git checkout master'
+cd
+cp .repo/manifests/requirements.txt /tmp/
+rm -rf $HOME/.repo $HOME/repo
 
-# mkdir -p wishful
-# cd wishful
-# repo init -u https://github.com/wishful-project/manifests.git
-# repo init -m user.xml
-# repo sync
-# repo forall -c 'git checkout master'
-# virtualenv -p /usr/bin/python3 ./dev
-# source ./dev/bin/activate
-# pip3 install -U -r ./.repo/manifests/requirements.txt
-# cd ..
+sudo apt-get install -y python3-dev python3-pip
+virtualenv -p /usr/bin/python3 ./dev
+. ./dev/bin/activate
+pip3 install -U -r /tmp/requirements.txt
+pip3 install pyric pyroute2 ipaddr netdiff scipy
+pip3 install networkx==1.11
